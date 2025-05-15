6c9f43771c14d5ad63914ef9b169441a51b82cb8
sukebenohito <linkpekerja@gmail.com>
2025-05-15 23:04:14 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+
+import "fmt"
+
+
+func (c *_Chatango) Message(user *_User, room *_Room, message *_Message) {
+	fmt.Printf("%s %s %s\n", room.Name, user.Name, message.Body)
+	if message.Body == "halo" {
+		room.Message("halo juga")
+	}
+	if message.Body == "out aja" {
+		room.Disconnect()
+	}
+}
+
+func (c *_Chatango) Connect(room *_Room) {
+	fmt.Printf("connected to %s\n", room.Name)
+}
+
+func (c *_Chatango) Disconnect(room *_Room, err error) {
+	fmt.Printf("disconnected to %s %s\n", room.Name, err)
+}
+
+
+func main(){
+	ch := Chatango()
+
+//	example
+//	ch.EasyStart([]string{"nico-nico"}, "BotName", "Password")
+	ch.EasyStart([]string{"nico-nico"}, "BotName", "")
+
+}
+
+
